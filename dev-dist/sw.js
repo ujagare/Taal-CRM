f8325a15d1e768fa263b5b741dda3db681938d36
DD Tech <Info@ddtech.in>
2026-07-25 20:18:22 +0530

    production ready: complete Supabase Auth setup with Google OAuth and mandatory login gate

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sq4usfr2vb"
+    "revision": "0.lklfuje40t4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
